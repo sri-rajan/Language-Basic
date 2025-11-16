16085298864af4392309ebcd2cbd56af0e1a4cb8
sri-rajan <sriraja781@gmail.com>
2025-11-17 04:06:03 +0530

    Hello World

---
+++ c++/initial/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// if we don't use namespace std ,we need to use std [standard] wherever we are using function like (cout..)
+using namespace std;
+
+int main(){
+  auto result = (10 <=> 20) > 0;
+  cout<<"Hello World";
+  return 0;
+}
+
+
+// OUTPUT:
+// Hello World
